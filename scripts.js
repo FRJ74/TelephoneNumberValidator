19d0cfa3be66a7277a0d69c9f6cc7cd47ebd14a3
FRJ74 <frankr74@gmail.com>
2025-04-17 14:23:52 -0600

    Add eventlistener & get input from html

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("user-input");
+const check = document.getElementById("check-btn");
+const clear = document.getElementById("clear-btn");
+const results = document.getElementById("result-div");
+
+check.addEventListener("click", () => {
+  if(!input.value) {
+    alert("Please provide a phone number")
+  }
+})
